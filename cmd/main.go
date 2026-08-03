26c4c94f91b471785b159aa768efff0a5f9de9da
mdev4648 <muledevs21@gmail.com>
2026-08-03 08:05:02 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	router := gin.Default() //creates a new HTTP server with useful defaults: This creates a router.
+
+	router.GET("/health", func(c *gin.Context) {  //This registers a route that responds to HTTP GET requests.
+		//in django like:  def health(request):
+		c.JSON(200, gin.H{
+			"status": "OK",
+		})
+
+	})
+
+	router.Run(":8080")
+}
